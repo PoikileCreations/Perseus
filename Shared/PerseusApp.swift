8c92b411f0e11ab0a968b7391801d0b94f8073ee
Jason R Tibbetts <jason@tibbetts.net>
2022-05-25 22:41:11 -0400

    [no ticket] [project] Created the new project.

---
+++ Shared/PerseusApp.swift
@@ -0,0 +1,20 @@
+//
+//  PerseusApp.swift
+//  Shared
+//
+//  Created by Jason R Tibbetts on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct PerseusApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
